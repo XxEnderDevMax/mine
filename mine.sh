d19ad4e3d66cbd24f85bf0067ac98c51429bca33
XxEnderDevMax <XxEnderDevMax@gmail.com>
2025-03-04 21:59:18 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz tar -xf xmrig-6.22.0-linux-static-x64.tar.gz 
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz && tar -xf xmrig-6.22.0-linux-static-x64.tar.gz 
 cd xmrig-6.22.0
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:TMncM9xrwghzpP7qA818jxxxiYaRGY7q5a.unmineable_worker_ponzovkq -p x 
